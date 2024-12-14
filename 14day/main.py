6a7778bc65845af503bada51bc5f02251fb715ef
David Burke <dburke@amberfire.net>
2024-12-14 13:07:38 -0500

    Day 14 Part 1

--- 14day/main.py
+++ 14day/main.py
@@ -6,7 +6,6 @@ import os
 import sys
 import re
 
-
 DEFAULT_FILE_NAME = "input.txt"
 SECONDS = 100
 
@@ -49,12 +48,36 @@ def get_file_name() -> str:
     return sys.argv[1]
 
 def get_position_and_velocity(file_data: list) -> list:
+    """
+    Extracts position and velocity data from a list of strings.
+
+    Args:
+        file_data (list): A list of strings, each containing numeric data.
+
+    Returns:
+        list: A list of lists, where each inner list contains integers representing
+        the extracted position and velocity data from each string in the input list.
+"""
     position_and_velocity = []
     for line in file_data:
         position_and_velocity.append([int(num) for num in extract_numbers(line)])
     return position_and_velocity
 
 def move_robots(position_and_velocity, is_test):
+    """
+    Moves robots based on their current position and velocity, updating their
+    positions over a fixed number of seconds. Determines the number of robots
+    in each of four quadrants of a grid and returns the product of these counts.
+
+    Parameters:
+        position_and_velocity (list of lists): A list where each element is a list
+            containing the x and y positions and velocities of a robot [x, y, vx, vy].
+        is_test (bool): A flag indicating whether to use test grid dimensions or
+            actual grid dimensions.
+
+    Returns:
+        int: The product of the number of robots in each of the four quadrants.
+    """
     if is_test:
         rows =7
         cols = 11
@@ -69,11 +92,8 @@ def move_robots(position_and_velocity, is_test):
         quad_2_4_x = range(51,101)
         quad_1_4_y = range(0,51)
         quad_2_3_y = range(52,103)
-        
-    quad = [0,0,0,0]
-    
+    quad = [0,0,0,0]    
     for robot in position_and_velocity:
-        print(f"Start = {robot[0]}, {robot[1]} Vel = {robot[2]},{robot[3]}")
         robot[0] = (robot[0] + robot[2] * SECONDS) % cols
         robot[1] = (robot[1] + robot[3] * SECONDS) % rows
         if robot[0] in quad_1_3_x and robot[1] in quad_1_4_y:
@@ -84,17 +104,14 @@ def move_robots(position_and_velocity, is_test):
             quad[3] += 1
         if robot[0] in quad_2_4_x and robot[1] in quad_2_3_y:
             quad[1] += 1
-        print(robot[0], robot[1])
-    print(quad)
     return quad[0] * quad[1] * quad[2] * quad[3]
-
-        
 
 def main():
     """ Main function
         Reads the specified input file
-        Splits the data into a 2d map - comment line if data is not a map
-        
+        Uses file name to determine if this is a test run
+        Gets a list of initial position and velocity
+        Processes list to obtain result
     """
     file_name = get_file_name()
     is_test =  "test" in file_name
